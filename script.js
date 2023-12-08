ab9a48b36fefdab4ff3d4a1febd54e1d4c5da7e0
Israel Rodrigues <israel28.developer@gmail.com>
2023-12-07 22:55:38 -0300

    Meu website

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: [ "Desenvolvedor Frontend","Desenvolvedor de Automação de Processos", "Especialista em Análise de Dados"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
